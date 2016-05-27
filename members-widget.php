8db82b8f7b69c3e3fabec95dccb378dd096d135e
Kevin W <gra230434@gmail.com>
2016-05-27 16:18:58 +0800

    member update

--- members-widget.php
+++ members-widget.php
@@ -32,7 +32,7 @@ class MembersPostWidget extends WP_Widget {
 	function widget( $args, $instance ) {
 		extract( $args );
 
-    $MWK_plugin_path = plugin_dir_path( __FILE__ );
+    $MWK_plugin_path = plugin_dir_url( __FILE__ );
 		/* Our variables from the widget settings. */
 		$MWK_name         = $instance['member_name'];
 		$MWK_introduction = $instance['member_introduction'];
@@ -44,33 +44,59 @@ class MembersPostWidget extends WP_Widget {
 		$MWK_google       = $instance['member_google'];
 		$MWK_instagram    = $instance['member_instagram'];
 		$MWK_linkedin     = $instance['member_linkedin'];
+		$MWK_image        = $instance['member_image'];
 
 		/* Before widget (defined by themes). */
 		echo $before_widget;
 
 		/* Display name from widget settings if one was input. */
-		echo $before_title . $MWK_name . $after_title;
-    $MWK_dividclass = '<div id="Members" class="MWK_Members_box">';
-		echo esc_attr($MWK_dividclass);
-		/* If show announcement was selected, display the announcement. */
-		if ( $introduction ) { echo '<p>' . $MWK_introduction . '</p>'; }
-		if ( $email ) { echo '<p>Email: <a href="mailto:' . $MWK_email . '">' . $MWK_email . '</a></p>'; }
-		if ( $number ) { echo '<p>Tel: ' . $MWK_number . '</p>'; }
 
-		if ( $MWK_facebook || $MWK_twitter || $MWK_google || $MWK_instagram || $MWK_linkedin ) {
-			echo "<ul class='MWK_member_social_ul'>";
-			if ( $facebook ) {echo "<li class='MWK_MS_FB MWK_MS_li'><a href=' . $MWK_facebook . '>Facebook</a></li>";}
-			if ( $twitter ) {echo "<li class='MWK_MS_TW MWK_MS_li'><a href=' . $MWK_twitter . '>Twitter</a></li>";}
-			if ( $google ) {echo "<li class='MWK_MS_G+ MWK_MS_li'><a href=' . $MWK_google . '>Twitter</a></li>";}
-			if ( $instagram ) {echo "<li class='MWK_MS_ins MWK_MS_li'><a href=' . $MWK_instagram . '>Twitter</a></li>";}
-			if ( $linkedin ) {echo "<li class='MWK_MS_IN MWK_MS_li'><a href=' . $MWK_linkedin . '>Twitter</a></li>";}
-			echo '</ul>';
-		}
+		if ( is_home() || is_font_page() ) {
+			if ( $MWK_image ) {
+				echo "<div class='MWK_home_image'><img src='" . $MWK_image . "'></div>";
+			} else {
+				echo "<div class='MWK_home_image'><img src='" . $MWK_plugin_path . "image/Person-icon-grey.JPG'></div>";
+			}
+			echo $before_title . $MWK_name . $after_title;
+			echo "<div id='Members' class='MWK_home_member'>";
+			if ( $MWK_email || $MWK_facebook || $MWK_twitter || $MWK_google || $MWK_instagram || $MWK_linkedin ) {
+	   		echo "<ul class='MWK_home_member_ul'>";
+				if ( $MWK_email     ) { echo "<li class='MWK_MS_EM MWK_MSh_li'><a href='mailto:"  . $MWK_email . "'><img src='" . $MWK_plugin_path . "image/mail-green.png'></a></li>"; }
+		  	if ( $MWK_facebook  ) { echo "<li class='MWK_MS_FB MWK_MSh_li'><a href='"      . $MWK_facebook . "'><img src='" . $MWK_plugin_path . "image/MWK_facebook.svg'></a></li>"; }
+  			if ( $MWK_twitter   ) { echo "<li class='MWK_MS_TW MWK_MSh_li'><a href='"       . $MWK_twitter . "'><img src='" . $MWK_plugin_path . "image/MWK_twitter.svg'></a></li>"; }
+	  		if ( $MWK_google    ) { echo "<li class='MWK_MS_G+ MWK_MSh_li'><a href='"        . $MWK_google . "'><img src='" . $MWK_plugin_path . "image/MWK_google_plus.svg'></li>"; }
+		  	if ( $MWK_instagram ) { echo "<li class='MWK_MS_ins MWK_MSh_li'><a href='"    . $MWK_instagram . "'><img src='" . $MWK_plugin_path . "image/MWK_instagram.svg'></li>"; }
+			  if ( $MWK_linkedin  ) { echo "<li class='MWK_MS_IN MWK_MSh_li'><a href='"      . $MWK_linkedin . "'><img src='" . $MWK_plugin_path . "image/MWK_linkedin.svg'></li>"; }
+		  	echo "<div class='MWK_clear'></div>";
+			  echo '</ul>';
+		  }
+			echo "</div>";
+		} else {
+			echo $before_title . $MWK_name . $after_title;
+		  echo "<div id='Members' class='MWK_Members_box'>";
+		/* If show announcement was selected, display the announcement. */
+	  	if ( $MWK_introduction ) { echo '<p>' . $MWK_introduction . '</p>'; }
+  		echo "<div class='MWK_link'>";
+  		if ( $MWK_email  ) { echo '<div class="MWK_Members_list"><img src="' . $MWK_plugin_path . 'image/mail-green.png"><a href="mailto:' . $MWK_email . '">' . $MWK_email . '</a></div>'; }
+	  	if ( $MWK_number ) { echo '<div class="MWK_Members_list"><img src="' . $MWK_plugin_path . 'image/telephone-blue.png">' . $MWK_number . '</div>'; }
+	  	echo "<div class='MWK_clear'></div>";
+		  echo "</div><!-- .MWK_Members_box -->";
+    /* if have social url */
+  		if ( $MWK_facebook || $MWK_twitter || $MWK_google || $MWK_instagram || $MWK_linkedin ) {
+	   		echo "<ul class='MWK_member_social_ul'>";
+		  	if ( $MWK_facebook  ) { echo "<li class='MWK_MS_FB MWK_MS_li'><a href=' . $MWK_facebook . '><img src='" . $MWK_plugin_path . "image/MWK_facebook.svg'></a></li>"; }
+  			if ( $MWK_twitter   ) { echo "<li class='MWK_MS_TW MWK_MS_li'><a href=' . $MWK_twitter . '>Twitter</a></li>"; }
+	  		if ( $MWK_google    ) { echo "<li class='MWK_MS_G+ MWK_MS_li'><a href=' . $MWK_google . '>Twitter</a></li>"; }
+		  	if ( $MWK_instagram ) { echo "<li class='MWK_MS_ins MWK_MS_li'><a href=' . $MWK_instagram . '>Twitter</a></li>"; }
+			  if ( $MWK_linkedin  ) { echo "<li class='MWK_MS_IN MWK_MS_li'><a href=' . $MWK_linkedin . '>Twitter</a></li>"; }
+		  	echo "<div class='MWK_clear'></div>";
+			  echo '</ul>';
+		  }
 
 		if ( $MWK_address ) { echo '<p>Address: ' . $MWK_address . '</p>'; }
-
+    echo "<div class='MWK_clear'></div>";
 		echo "</div>";
-
+	  }
 		/* After widget (defined by themes). */
 		echo $after_widget;
 	}
@@ -84,12 +110,13 @@ class MembersPostWidget extends WP_Widget {
 		$instance['member_introduction'] = esc_textarea( $new_instance['member_introduction'] );
 		$instance['member_email']        = strip_tags( $new_instance['member_email'] );
 		$instance['member_phone_number'] = strip_tags( $new_instance['member_phone_number'] );
+		$instance['member_address']      = strip_tags( $new_instance['member_address'] );
 		$instance['member_facebook']     = esc_url( $new_instance['member_facebook'] );
 		$instance['member_twitter']      = esc_url( $new_instance['member_twitter'] );
 		$instance['member_google']       = esc_url( $new_instance['member_google'] );
 		$instance['member_instagram']    = esc_url( $new_instance['member_instagram'] );
 		$instance['member_linkedin']     = esc_url( $new_instance['member_linkedin'] );
-		$instance['member_address']      = strip_tags( $new_instance['member_address'] );
+		$instance['member_image']        = esc_url( $new_instance['member_image'] );
 
 		return $instance;
 	}
@@ -109,16 +136,18 @@ class MembersPostWidget extends WP_Widget {
 			'member_google'       => '',
 			'member_instagram'    => '',
 			'member_linkedin'     => '',
+			'member_image'        => '',
 		   );
 		$instance = wp_parse_args( (array) $instance, $defaults );
-		$mem_email     = ($instance['member_email']=='')        ? 'placeholder="enter email"'               : 'value="' . $instance['member_email'] . '"';
-		$mem_phone     = ($instance['member_phone_number']=='') ? 'placeholder="enter phone number"'        : 'value="' . $instance['member_phone_number'] . '"';
-		$mem_address   = ($instance['member_address']=='')      ? 'placeholder="enter address"'             : 'value="' . $instance['member_address'] . '"';
-		$mem_facebook  = ($instance['member_facebook']=='')     ? 'placeholder="enter facebook"'            : 'value="' . $instance['member_facebook'] . '"';
-		$mem_twitter   = ($instance['member_twitter']=='')      ? 'placeholder="enter twitter"'             : 'value="' . $instance['member_twitter'] . '"';
-		$mem_google    = ($instance['member_google']=='')       ? 'placeholder="enter google+"'             : 'value="' . $instance['member_google'] . '"';
-		$mem_instagram = ($instance['member_instagram']=='')    ? 'placeholder="enter instagram"'           : 'value="' . $instance['member_instagram'] . '"';
-		$mem_linkedin  = ($instance['member_linkedin']=='')     ? 'placeholder="enter linkedin"'            : 'value="' . $instance['member_linkedin'] . '"';
+		$mem_email     = ($instance['member_email']=='')        ? 'placeholder="enter email"'        : 'value="' . $instance['member_email'] . '"';
+		$mem_phone     = ($instance['member_phone_number']=='') ? 'placeholder="enter phone number"' : 'value="' . $instance['member_phone_number'] . '"';
+		$mem_address   = ($instance['member_address']=='')      ? 'placeholder="enter address"'      : 'value="' . $instance['member_address'] . '"';
+		$mem_facebook  = ($instance['member_facebook']=='')     ? 'placeholder="enter facebook"'     : 'value="' . $instance['member_facebook'] . '"';
+		$mem_twitter   = ($instance['member_twitter']=='')      ? 'placeholder="enter twitter"'      : 'value="' . $instance['member_twitter'] . '"';
+		$mem_google    = ($instance['member_google']=='')       ? 'placeholder="enter google+"'      : 'value="' . $instance['member_google'] . '"';
+		$mem_instagram = ($instance['member_instagram']=='')    ? 'placeholder="enter instagram"'    : 'value="' . $instance['member_instagram'] . '"';
+		$mem_linkedin  = ($instance['member_linkedin']=='')     ? 'placeholder="enter linkedin"'     : 'value="' . $instance['member_linkedin'] . '"';
+		$mem_image     = ($instance['member_image']=='')        ? 'placeholder="enter img url"'      : 'value="' . $instance['member_image'] . '"';
 		?>
 
 <?php // input name ?>
@@ -166,6 +195,11 @@ class MembersPostWidget extends WP_Widget {
 			<label for="<?php echo $this->get_field_id( 'member_linkedin' ); ?>">Twitter:</label>
 			<input id="<?php echo $this->get_field_id( 'member_linkedin' ); ?>" name="<?php echo $this->get_field_name( 'member_linkedin' ); ?>" <?php echo $mem_linkedin; ?> style='width:100%;' type='url'>
 		</p>
+<?php // input image url ?>
+    <p>
+    	<label for="<?php echo $this->get_field_id( 'member_image' ); ?>">Image url:</label>
+     	<input id="<?php echo $this->get_field_id( 'member_image' ); ?>" name="<?php echo $this->get_field_name( 'member_image' ); ?>" <?php echo $mem_image; ?> style='width:100%;' type='url'>
+    </p>
 <?php // input summary ?>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'member_introduction' ); ?>">Summary:</label>
@@ -179,5 +213,12 @@ class MembersPostWidget extends WP_Widget {
 	<?php
 	}
 }
+
+function members_widget_style(){
+	# code...
+	wp_enqueue_style('members_widget', plugin_dir_url( __FILE__ ) . 'css/MWK_widget.css');
+}
+
+add_action( 'wp_enqueue_scripts', 'members_widget_style' );
 
 ?>
